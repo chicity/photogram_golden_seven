6137bdf51d5c1b283d4f41a209b4bb6845857fe0
Will <will.gossin@chicagobooth.edu>
2017-07-20 01:39:48 +0000

    foolish attempt

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,44 @@
 class PicturesController < ApplicationController
     def new_form
-        render("pic_templates/new_form.html.erb")
+   
+    # here are the parameters: {"an_id"=>"5"} -- "an_id" came from when we assigned it in the routes
+    the_id_number = params["an_id"] #what does params mean again?
+    pic = Photo.find(the_id_number) #looks up 
+    
+    @the_source = pic.source
+    @the_caption = pic.caption
+    
+    #alt: @pic = Photo.find(parms["an_id]") // in View template source changes
+    render("pic_templates/new_form.html.erb")
     end
+    
+    def create_row
+    
+
+    p = Photo.new
+    p.source = params["the_source"]
+    p.caption = params["the_caption"]
+    p.save 
+    
+    @photo_count = Photo.count
+    render("pic_templates/create_row.html.erb")
+
+    end
+
+    def index
+    
+    
+        render("pic_templates/index.html.erb")
+
+    end
+    
+    def show
+    
+        
+        render("pic_templates/index.html.erb")
+    end
+
+
+    
+    
 end
